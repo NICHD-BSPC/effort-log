324fcb93784df1fe426eb68b0dfafdda707d3be6
Ryan Dale <ryan.dale@nih.gov>
2023-01-20 22:54:14 -0500

    run init if needed

--- app/prestart.sh
+++ app/prestart.sh
@@ -2,5 +2,9 @@
 
 # The Docker container will look for this specially-named file and run it
 # before starting the app.
+
+if [ ! -e migrations/ ]; then
+    flask db init
+fi
 flask db migrate || echo "migrate failed with exit code $?"
 flask db upgrade
