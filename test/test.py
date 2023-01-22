a892e098d43c80879fb7e1bec97d10a282150370
Ryan Dale <ryan.dale@nih.gov>
2023-01-22 16:27:41 -0500

    more fine-tuning tests

--- test/test.py
+++ test/test.py
@@ -1,13 +1,12 @@
 """
 Usage:
 
-docker compose --env-file app/.env up -d
+    docker compose --env-file app/.env -f compose.yml -f compose-test.yml up -d
+    python test/test.py
 
-Then 
+Then to stop:
 
-to stop:
-
-docker compose --env-file app/.env down
+    docker compose --env-file app/.env -f compose.yml -f compose-test.yml down
 
 """
 
@@ -17,11 +16,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Remote(
-   command_executor='http://localhost:4444',
-   options=webdriver.FirefoxOptions()
+    # This is the port configured in compose-test.yml for the selenium
+    # container
+    command_executor='http://localhost:4444',
+    options=webdriver.FirefoxOptions()
 )
+
+# Since it's the selenium container that's doing the GET, this is *within the
+# docker network*, which uses hostnames as configured in docker compose.
 driver.get("http://web:80")
+
+# Save an initial screenshot
 driver.save_screenshot('1.png')
+
+# Click the "Add entry" button from the home page
 add_entry_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Add entry')]")
 add_entry_button.click()
 
@@ -37,18 +45,22 @@ def check_options(element, expected):
 proj_select = Select(driver.find_element(By.XPATH, "//select[@id='project']"))
 assert proj_select.options == []
 
-# Select a lab
+# Confirm the set of selection, and choose lab2
 pi_select = Select(driver.find_element(By.XPATH, "//select[@id='pi']"))
 check_options(pi_select, ["---", "lab1", "lab2", "other"])
 pi_select.select_by_visible_text("lab2")
 
-# Now the project should be populated
+# Now the project dropdown should be populated
 check_options(proj_select, ["projectC", "projectD", "projectE", "other"])
+
+# Choose projectD
 proj_select.select_by_visible_text("projectD")
 
 # Add a note
 element = driver.find_element(By.XPATH, "//textarea")
 element.send_keys("duly noted")
+
+# Save a screenshot of the filled-in form before submitting
 driver.save_screenshot('3.png')
 
 # Submit form
@@ -56,5 +68,7 @@ driver.find_element(By.XPATH, "//input[@id='submit']").click()
 
 # Ensure our just-added entry exists
 driver.find_element(By.XPATH, "//td[contains(text(), 'duly noted')]")
+
+# and take a screenshot of the home page with table and plot.
 driver.save_screenshot('4.png')
 driver.close()
