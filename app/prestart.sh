4709c2b4f0c8f50ddb2823c47e3f949b07913943
Ryan Dale <ryan.dale@nih.gov>
2023-06-12 20:26:35 -0400

    support downloading LDAP certificate in prestart

--- app/prestart.sh
+++ app/prestart.sh
@@ -3,8 +3,15 @@
 # The Docker container will look for this specially-named file and run it
 # before starting the app.
 
+# 
+if [ ! -z "$CERT_URL" && ! -z "$LDAP_CERT" ]; then
+    curl -O "$CERT_URL" > "$LDAP_CERT"
+    chmod ugo+r "$LDAP_CERT"
+fi
+
 if [ ! -e migrations/ ]; then
     flask db init
 fi
 flask db migrate || echo "migrate failed with exit code $?"
 flask db upgrade
+
